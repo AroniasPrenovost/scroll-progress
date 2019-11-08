cd5b11fc59b5a1b8c429bbe68143e7967d3a01fd
Aron Prenovost <Aroniascoder@gmail.com>
2019-11-07 20:52:09 -0800

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,7 +1,19 @@
 var progressBar = document.createElement('div');
-progressBar.style.cssText = ' position:fixed;top:0;width:1%;height:4px;z-index:100;background:#2980b9';
+progressBar.style.cssText = 'transition: width 1s;position:fixed;top:0;width:1%;height:3px;z-index:100;background:#2980b9';
 document.body.appendChild(progressBar);
 
+// resource load percentage progress bar 
+var stateCheck = setInterval(() => {
+    if (document.readyState === 'complete') {
+        clearInterval(stateCheck);
+        progressBar.style.width = '100%';
+    } else {
+        progressBar.style.width = '55%';
+    }
+}, 1);
+
+// get location-based progress bar on scroll 
+/*
 getScrollTopMax = function () {
     var ref;
     return (ref = document.scrollingElement.scrollTopMax) != null
@@ -10,8 +22,8 @@ getScrollTopMax = function () {
 };
 
 var scrollTopMax = getScrollTopMax();
-
 window.addEventListener('scroll', function () {
     var scrollPositionPercentage = Math.floor((window.scrollY / scrollTopMax) * 100);
     progressBar.style.width = scrollPositionPercentage + '%';
 });
+*/
